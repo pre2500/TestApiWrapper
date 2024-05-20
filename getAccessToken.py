967972193e9b5e93c8dbb84d5bcb1d8adfff2c99
praveen.ezhil <praveen.ezhil@bytedance.com>
2024-05-20 02:02:12 -0700

    first commit

---
+++ getAccessToken.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_access_token(client_access_key, client_secret_key):
+    """Get Access Token function will take in client access and client secret to return a access token
+
+    Parameters
+    ----------
+    client_access_key :
+        param client_secret_key:
+    client_secret_key :
+        
+
+    Returns
+    -------
+
+    """
